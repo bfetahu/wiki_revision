abd298ded5e073925a3d8cb1c680238c18280b5d
ChristophHubeL3S <hube@l3s.de>
2017-02-13 16:55:03 +0100

    diverse changes

--- src/main/java/revisions/RevisionReader.java
+++ src/main/java/revisions/RevisionReader.java
@@ -17,12 +17,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.StringReader;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static utils.Utils.extractReferences;
+import static utils.Utils.getJaccardDistance;
 
 /**
  * Created by hube on 1/20/2017.
@@ -33,15 +31,27 @@ public class RevisionReader {
     WikipediaEntityRevision lastRevision = new WikipediaEntityRevision();
 
     boolean firstRevision = true;
+    NLPUtils nlp = new NLPUtils(2);
+    Set<String> stopWords = new HashSet<String>();
+    String user_name = "";
+
+    FileWriter fw = new FileWriter("C:\\Users\\hube\\bias\\generated_datasets\\malaysia_airlins_17_sections");
+
+    public RevisionReader() throws IOException {
+    }
 
 
     public void readRevisions(String revisionsFile) throws Exception {
 
-        BufferedReader reader = new BufferedReader(new FileReader(revisionsFile));
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(revisionsFile), "UTF8"));
+
+        readStopWordList("C:\\Users\\hube\\bias\\datasets\\stop_words");
+
         String line = "";
         String revision_id = "";
         String user_id = "";
-        String user_name = "";
         String revision_comment = "";
         String timestamp = "";
         String entity_title = "";
@@ -51,6 +61,8 @@ public class RevisionReader {
 
 
         while ((line = reader.readLine()) != null && counter < 5000) {
+
+            //System.out.println(line);
 
             WikipediaEntityRevision revision = new WikipediaEntityRevision();
 
@@ -117,25 +129,18 @@ public class RevisionReader {
 
             //sections
             Set<String> section_keys = entity.getSectionKeys(); //all section names of the article
-            WikiSection main_section = entity.getSections();
 
             for (String section : section_keys) {
-
-                WikiSection sub_section = main_section.findSection(section);
+                WikiSection sub_section = entity.getSection(section);
 
                 //summary
-                //System.out.println(user_id + "\t" + user_name + "\t" + entity_id + "\t" + entity_title + "\t"
-                //        + revision_id + "\t" + revision_comment + "\t" + timestamp + "\t" + section);
-
-                //TODO: content added to section, content removed from section, sources added to section, sources removed from section
+//                System.out.println(user_id + "\t" + user_name + "\t" + entity_id + "\t" + entity_title + "\t"
+//                        + revision_id + "\t" + revision_comment + "\t" + timestamp + "\t" + section);
             }
 
-            if (firstRevision) {
-                firstRevision = false;
-            } else {
-                //compare current revision with last one
-                compareWithOldRevision(revision);
-            }
+            compareWithOldRevision(revision);
+
+            if (firstRevision) firstRevision = false;
 
             //current revision becomes last revision
             lastRevision = revision;
@@ -144,13 +149,12 @@ public class RevisionReader {
     }
 
 
-    public void compareWithOldRevision(WikipediaEntityRevision revision) {
+    public void compareWithOldRevision(WikipediaEntityRevision revision) throws IOException {
 
         WikipediaEntity entity = revision.entity;
 
         Set<String> section_keys = entity.getSectionKeys(); //all section names of the article, also subsections
         Set<String> old_section_keys = lastRevision.entity.getSectionKeys();
-        WikiSection main_section = entity.getSections();
 
         //compare sections with their counterpart in the old revision, for now: only exact matches
 
@@ -159,8 +163,6 @@ public class RevisionReader {
             //System.out.println(section);
             if (old_section_keys.contains(section)) {
 
-                //TODO: section name could have be changed, look for similar sections (text similarity measure)
-
                 for (String old_section : old_section_keys) {
                     if (old_section.equals(section)) {
                         compareTwoSections(entity, section, old_section, revision);
@@ -168,9 +170,31 @@ public class RevisionReader {
                     }
                 }
             } else {
-                //new section
-                System.out.println("New section: " + section + " added by user: " + revision.user_id
-                        + " in revision: " + revision.revision_id);
+                //check if section name has been changed
+                String mostSimilarSection = "";
+                double mostSimilarSectionDistance = 1.0;
+
+                if (!firstRevision) {
+                    for (String old_section : old_section_keys) {
+                        //compare section texts
+                        double jacdis = getJaccardDistance(revision.entity.getSectionText(section), lastRevision.entity.getSectionText(old_section));
+                        if (jacdis < 0.5 && jacdis < mostSimilarSectionDistance) {
+                            mostSimilarSection = old_section;
+                            mostSimilarSectionDistance = jacdis;
+                        }
+                    }
+                }
+
+                if (!mostSimilarSection.equals("")){
+                    compareTwoSections(entity, section, mostSimilarSection, revision);
+                    break;
+                } else{
+                    //new section
+                    compareTwoSections(entity, section, "", revision); //compare with blank
+
+//                    System.out.println("New section: " + section + " added by user: " + revision.user_id
+//                            + " in revision: " + revision.revision_id);
+                }
             }
         }
 
@@ -180,14 +204,14 @@ public class RevisionReader {
                 continue;
             } else {
                 //deleted section
-                System.out.println("Deleted section: " + old_section + " deleted by user: " + revision.user_id
-                        + " in revision: " + revision.revision_id);
+                //System.out.println("Deleted section: " + old_section + " deleted by user: " + revision.user_id
+                //        + " in revision: " + revision.revision_id);
             }
         }
     }
 
 
-    public void compareTwoSections(WikipediaEntity entity, String section_title, String old_section_title, WikipediaEntityRevision revision) {
+    public void compareTwoSections(WikipediaEntity entity, String section_title, String old_section_title, WikipediaEntityRevision revision) throws IOException {
         //retrieve the sections
         String section_text = entity.getSectionText(section_title);
 
@@ -200,7 +224,12 @@ public class RevisionReader {
             System.out.println("Section " + section_title + " does not exist!");
         }
 
-        String old_section_text = lastRevision.entity.getSectionText(old_section_title);
+        String old_section_text;
+        if (old_section_title.equals("")){ //this is the case when the section is new
+            old_section_text = "";
+        } else{
+            old_section_text = lastRevision.entity.getSectionText(old_section_title);
+        }
 
         if (section_text == null){
             System.out.println("section_text is NULL!");
@@ -214,30 +243,88 @@ public class RevisionReader {
         //citations
         Set<String> referencesAdded = getReferencesAddedOrRemoved(section_text, old_section_text);
         Set<String> referencesRemoved = getReferencesAddedOrRemoved(old_section_text, section_text);
-        if (referencesAdded.size() > 0) System.out.println("References added: " + referencesAdded
-            + " by user: " + revision.user_id + " in revision: " + revision.revision_id);
-        if (referencesRemoved.size() > 0) System.out.println("References removed: " + referencesRemoved
-                + " by user: " + revision.user_id + " in revision: " + revision.revision_id);
+        //if (referencesAdded.size() > 0) System.out.println("References added: " + referencesAdded
+        //    + " by user: " + revision.user_id + " in revision: " + revision.revision_id);
+        //if (referencesRemoved.size() > 0) System.out.println("References removed: " + referencesRemoved
+        //        + " by user: " + revision.user_id + " in revision: " + revision.revision_id);
 
         //text
-        Set<String> textAdded = getTextAdded(section_text, old_section_text);
+        Set<String> textAdded = getTextDifferences(section_text, old_section_text);
+        Set<String> textRemoved = getTextDifferences(old_section_text, section_text);
 
+        //if (textAdded.size() > 0) System.out.println("old : " + old_section_text + "\nnew: " + section_text + "\n" + textAdded);
+        //if (textAdded.size() > 0) System.out.println("textAdded: " + textAdded);
+        //if (textRemoved.size() > 0) System.out.println("textRemoved: " + textRemoved);
+
+        //testing
+        if (section_title.equals("") || section_title.equals(" ")) {
+            System.out.println("empty title");
+        }
+
+//        System.out.println(revision.user_id + "\t" + user_name + "\t" + revision.entity_id + "\t" + section_title + "\t" + revision.revision_id
+//                + "\t" + revision.timestamp + "\t" + revision.revision_comment + "\t" + referencesAdded + "\t" + referencesRemoved
+//                + "\t" + textAdded + "\t" + textRemoved);
+//        fw.write(revision.user_id + "\t" + user_name + "\t" + revision.entity_id + "\t" + section_title + "\t" + revision.revision_id
+//                + "\t" + revision.timestamp + "\t" + revision.revision_comment + "\t" + referencesAdded + "\t" + referencesRemoved
+//                + "\t" + textAdded + "\t" + textRemoved + "\n");
+//        fw.write(section_title + "\t" + revision.revision_id + "\t" + StringEscapeUtils.escapeJson(section_text) + "\n");
+//        fw.flush();
     }
 
 
-    public Set<String> getTextAdded(String new_text, String old_text) {
+    //compare two section texts
+    public Set<String> getTextDifferences(String text1, String text2) {
         Set<String> different_text = new HashSet<>();
 
-        //check what text new_text contains that is not contained in old_text
+        //check what text text1 contains that is not contained in text2
 
-        NLPUtils nlp = new NLPUtils(2);
-        List<String> sentences_text1 = nlp.getDocumentSentences(new_text);
-        List<String> sentences_text2 = nlp.getDocumentSentences(old_text);
+        //escape instead?
+        text1 = text1.replace("�","");
+        text2 = text2.replace("�","");
+        text1 = text1.replaceAll("\\{\\{[0-9]+}}", "");
+        text2 = text2.replaceAll("\\{\\{[0-9]+}}", "");
 
-        for (String sentence : sentences_text1){
-            if (!sentences_text2.contains(sentence)){
-                //sentence has been added(removed), changed or moved from(to) another section
-                //TODO: check if there is a similar sentence
+        List<String> sentences_text1 = nlp.getDocumentSentences(text1);
+        List<String> sentences_text2 = nlp.getDocumentSentences(text2);
+
+        for (String sentence_text1 : sentences_text1){
+
+            Set<String> sentence_words_text1 = genereateWordBag(sentence_text1);
+
+            if (!sentences_text2.contains(sentence_text1)){ //does not work for all cases
+                //sentence has been added, changed or moved from another section
+
+                //check if there is a similar sentence in the old revision. If there are multiple ones, pick the most similar one.
+                String mostSimilarSentence = "";
+                double mostSimilarSentenceDis = 1.0;
+
+                for (String sentence_text2 : sentences_text2){
+                    double jacdis = getJaccardDistance(sentence_text1,sentence_text2);
+
+                    if (jacdis <= 0.5 && jacdis < mostSimilarSentenceDis){
+                        mostSimilarSentence = sentence_text2;
+                        mostSimilarSentenceDis = jacdis;
+                    }
+                }
+                if (!mostSimilarSentence.equals("")) {
+                    //System.out.println("old: " + mostSimilarSentence + "\n" + "new: " + sentence_text1);
+
+                    Set<String> mostSimilarSentence_words = genereateWordBag(mostSimilarSentence);
+
+                    for (String word : sentence_words_text1){
+                        if (!mostSimilarSentence_words.contains(word)){
+                                different_text.add(word);
+                        }
+                    }
+                } else{
+                    //no similar sentence found --> new (removed) sentence
+                    for (String word : sentence_words_text1){
+                        different_text.add(word);
+                    }
+                }
+            } else{
+                //nothing has been added or removed
+                //System.out.println("No changes");
             }
         }
 
@@ -245,8 +332,22 @@ public class RevisionReader {
     }
 
 
-    public Set<String> getTextRemoved(String new_text, String old_text){
-        return null;
+    public Set<String> genereateWordBag(String sentence){
+
+        Set<String> word_bag = new HashSet<>();
+
+        //clean
+        sentence = sentence.replace("\n"," ");
+        sentence = sentence.replaceAll("[\\[\\].:,!?;()\"\'{}|=/<>+*]"," ");
+        sentence = sentence.toLowerCase();
+
+        String[] sentence_split = sentence.split(" ");
+        for (String word : sentence_split){
+            word = word.replaceAll("\\s+","");
+            if (!stopWords.contains(word) && !word.equals("")) word_bag.add(word);
+        }
+
+        return word_bag;
     }
 
 
@@ -272,10 +373,10 @@ public class RevisionReader {
         DocumentBuilder builder = factory.newDocumentBuilder();
         // InputSource is = new InputSource(new FileReader("C:\\Users\\hube\\Desktop\\mh17.xml"));
         InputSource is = new InputSource(new StringReader(xml));
-        //System.out.println(xml);
+
         DOMParser parser = new DOMParser();
         xml = xml.replaceAll("^(.*?)$", "");
-        //System.out.println(xml);
+
         Document doc = null;
         try {
             parser.parse(new InputSource(new java.io.StringReader((xml))));
@@ -286,6 +387,19 @@ public class RevisionReader {
         }
 
         return doc;
+    }
+
+
+    public void readStopWordList(String file) throws IOException {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(file), "UTF8"));
+
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            stopWords.add(line);
+        }
     }
 
 
