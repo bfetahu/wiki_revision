2e3b583de3bb6964e669fcd494339baee2ac9ad5
besnik <bfetahu@gmail.com>
2017-02-14 11:37:26 +0100

    xxx

--- src/main/java/revisions/SectionClusterer.java
+++ src/main/java/revisions/SectionClusterer.java
@@ -1,5 +1,6 @@
 package revisions;
 
+import entities.WikiSection;
 import entities.WikipediaEntity;
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.battelle.clodhopper.Cluster;
@@ -30,7 +31,7 @@ public class SectionClusterer {
 
         Set<String> stop_words = FileUtils.readIntoSet(stop_words_file, "\n", false);
         Map<Long, Set<String>> revision_sections = new HashMap<>();
-        Map<String, StringBuffer> sections = is_raw_revision_file ? readSectionsFromRawRevisions(input_file, revision_sections) : readProcessedSectionRevisions(input_file, revision_sections);
+        Map<Integer, Map<String, StringBuffer>> sections = is_raw_revision_file ? readSectionsFromRawRevisions(input_file, revision_sections) : readProcessedSectionRevisions(input_file, revision_sections);
 
         //compute the average number of sections across all revisions
         int min_section_num = revision_sections.values().stream().mapToInt(x -> x.size()).min().getAsInt();
@@ -49,9 +50,11 @@ public class SectionClusterer {
      * @return
      * @throws IOException
      */
-    public static Map<String, StringBuffer> readProcessedSectionRevisions(String file, Map<Long, Set<String>> revision_sections) throws IOException {
+    public static Map<Integer, Map<String, StringBuffer>> readProcessedSectionRevisions(String file, Map<Long, Set<String>> revision_sections) throws IOException {
         //read the sections into the map data structure.
-        Map<String, StringBuffer> sections = new HashMap<>();
+        Map<Integer, Map<String, StringBuffer>> sections = new HashMap<>();
+        Map<String, StringBuffer> sub_sections = new HashMap<>();
+        sections.put(0, sub_sections);
         BufferedReader reader = FileUtils.getFileReader(file);
         while (reader.ready()) {
             String section_revision_line = reader.readLine().trim();
@@ -64,10 +67,10 @@ public class SectionClusterer {
             String section_title = data[0].trim().toLowerCase();
             long rev_id = Long.valueOf(data[1]);
             String section_text = StringEscapeUtils.unescapeJson(data[2]);
-            if (!sections.containsKey(section_title)) {
-                sections.put(section_title, new StringBuffer());
+            if (!sub_sections.containsKey(section_title)) {
+                sub_sections.put(section_title, new StringBuffer());
             }
-            sections.get(section_title).append(section_text).append("\n");
+            sub_sections.get(section_title).append(section_text).append("\n");
 
             if (!revision_sections.containsKey(rev_id)) {
                 revision_sections.put(rev_id, new HashSet<>());
@@ -83,8 +86,8 @@ public class SectionClusterer {
      * @param file
      * @return
      */
-    public static Map<String, StringBuffer> readSectionsFromRawRevisions(String file, Map<Long, Set<String>> revision_sections) throws IOException {
-        Map<String, StringBuffer> sections = new HashMap<>();
+    public static Map<Integer, Map<String, StringBuffer>> readSectionsFromRawRevisions(String file, Map<Long, Set<String>> revision_sections) throws IOException {
+        Map<Integer, Map<String, StringBuffer>> sections = new HashMap<>();
         BufferedReader reader = FileUtils.getFileReader(file);
         String line;
 
@@ -111,12 +114,20 @@ public class SectionClusterer {
             //extract the references.
             for (String section : entity.getSectionKeys()) {
                 String section_text = entity.getSectionText(section);
+                WikiSection wiki_section = entity.getSection(section);
 
                 String section_label = section.toLowerCase().replaceAll("\\{\\{(.*?)\\}\\}", "").replaceAll("<ref>(.*)</?ref>", "").trim();
-                if (!sections.containsKey(section_label)) {
-                    sections.put(section_label, new StringBuffer());
+                if (section_label.isEmpty()) {
+                    continue;
                 }
-                sections.get(section_label).append(section_text).append("\n");
+
+                if (!sections.containsKey(wiki_section.section_level)) {
+                    sections.put(wiki_section.section_level, new HashMap<>());
+                }
+                if (!sections.get(wiki_section.section_level).containsKey(section_label)) {
+                    sections.get(wiki_section.section_level).put(section_label, new StringBuffer());
+                }
+                sections.get(wiki_section.section_level).get(section_label).append(section_text).append("\n");
 
                 if (!revision_sections.containsKey(rev_id)) {
                     revision_sections.put(rev_id, new HashSet<>());
@@ -130,7 +141,7 @@ public class SectionClusterer {
     /**
      * Perform XMeans clustering on the section labels.
      */
-    private static String clusterSections(Map<String, StringBuffer> sections, Set<String> stop_words, int min_num_clusters, int max_num_clusters) {
+    private static String clusterSections(Map<Integer, Map<String, StringBuffer>> sections, Set<String> stop_words, int min_num_clusters, int max_num_clusters) {
         SimilarityMeasures.stop_words = stop_words;
         XMeansParams xparams = new XMeansParams();
         xparams.setMinClusters(min_num_clusters);
@@ -138,65 +149,67 @@ public class SectionClusterer {
         xparams.setWorkerThreadCount(500);
         xparams.setUseOverallBIC(true);
 
+        StringBuffer sb = new StringBuffer();
         try {
-            String[] section_headlines = new String[sections.size()];
-            sections.keySet().toArray(section_headlines);
-            double[][] scores = new double[section_headlines.length][section_headlines.length];
-            IntStream.range(0, section_headlines.length).parallel().forEach(i -> {
-                for (int j = i; j < section_headlines.length; j++) {
-                    double score = 0.0;
-                    if (i == j) {
-                        score = 1.0;
-                    } else {
-                        score = SimilarityMeasures.computeCosineSimilarity(sections.get(section_headlines[i]).toString(), sections.get(section_headlines[j]).toString());
+            for (int section_level : sections.keySet()) {
+                String[] section_headlines = new String[sections.get(section_level).size()];
+                sections.get(section_level).keySet().toArray(section_headlines);
+                double[][] scores = new double[section_headlines.length][section_headlines.length];
+                IntStream.range(0, section_headlines.length).parallel().forEach(i -> {
+                    for (int j = i; j < section_headlines.length; j++) {
+                        double score = 0.0;
+                        if (i == j) {
+                            score = 1.0;
+                        } else {
+                            score = SimilarityMeasures.computeCosineSimilarity(sections.get(section_level).get(section_headlines[i]).toString(), sections.get(section_level).get(section_headlines[j]).toString());
+                        }
+                        scores[i][j] = score;
                     }
-                    scores[i][j] = score;
-                }
-            });
+                });
 
-            //update the similarities of the lower triangle of the matrix
-            for (int i = 0; i < scores.length; i++) {
-                for (int j = 0; j < i; j++) {
-                    scores[i][j] = scores[j][i];
+                //update the similarities of the lower triangle of the matrix
+                for (int i = 0; i < scores.length; i++) {
+                    for (int j = 0; j < i; j++) {
+                        scores[i][j] = scores[j][i];
+                    }
+                }
+                TupleList tpl = new Array2DTupleList(scores);
+                System.out.println("Starting to compute the xmeans clusters");
+
+                XMeansClusterer xmeans = new XMeansClusterer(tpl, xparams);
+                Thread th = new Thread(xmeans);
+                th.start();
+                List<Cluster> clusters = xmeans.get();
+
+                int cluster_id = 1;
+                //convert the keys to a set
+                Map<Integer, Set<String>> cluster_instances = new HashMap<>();
+
+                for (Cluster cluster : clusters) {
+                    Set<String> sub_cluster_instances = new HashSet<>();
+                    cluster_instances.put(cluster_id, sub_cluster_instances);
+
+                    IntIterator sections_indices = cluster.getMembers();
+                    while (sections_indices.hasNext()) {
+                        int section_index = sections_indices.getNext();
+                        String section = section_headlines[section_index];
+                        sub_cluster_instances.add(section);
+                    }
+                    cluster_id++;
+                }
+
+                for (int cluster_id_key : cluster_instances.keySet()) {
+                    String clustered_sections = "";
+                    for (String section : cluster_instances.get(cluster_id_key)) {
+                        clustered_sections = clustered_sections + section + ";";
+                    }
+                    sb.append(section_level).append("\t").append(cluster_id_key).append("\t").append(clustered_sections).append("\n");
                 }
             }
-            TupleList tpl = new Array2DTupleList(scores);
-            System.out.println("Starting to compute the xmeans clusters");
 
-            XMeansClusterer xmeans = new XMeansClusterer(tpl, xparams);
-            Thread th = new Thread(xmeans);
-            th.start();
-            List<Cluster> clusters = xmeans.get();
-
-            int cluster_id = 1;
-            //convert the keys to a set
-            Map<Integer, Set<String>> cluster_instances = new HashMap<>();
-
-            for (Cluster cluster : clusters) {
-                Set<String> sub_cluster_instances = new HashSet<>();
-                cluster_instances.put(cluster_id, sub_cluster_instances);
-
-                IntIterator sections_indices = cluster.getMembers();
-                while (sections_indices.hasNext()) {
-                    int section_index = sections_indices.getNext();
-                    String section = section_headlines[section_index];
-                    sub_cluster_instances.add(section);
-                }
-                cluster_id++;
-            }
-
-            StringBuffer sb = new StringBuffer();
-            for (int cluster_id_key : cluster_instances.keySet()) {
-                String clustered_sections = "";
-                for (String section : cluster_instances.get(cluster_id_key)) {
-                    clustered_sections = clustered_sections + section + ";";
-                }
-                sb.append(cluster_id_key).append("\t").append(clustered_sections).append("\n");
-            }
-            return sb.toString();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "";
+        return sb.toString();
     }
 }
--- src/main/java/revisions/TestClass.java
+++ src/main/java/revisions/TestClass.java
@@ -1,8 +1,6 @@
 package revisions;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by hube on 1/27/2017.
@@ -10,7 +8,6 @@ import java.util.Map;
 public class TestClass {
 
     public static void main(String[] args) throws IOException {
-        Map<String, StringBuffer> sections = SectionClusterer.readSectionsFromRawRevisions("/Users/besnik/Desktop/top_1.xml", new HashMap<>());
-        System.out.println(sections.keySet());
+
     }
 }
